829e84cfbe4df5491a956819149c35f31637bdae
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-02 04:49:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 class Student
 
-  attr_accessor :name, :grade
+  attr_accessor :name, :grade, :id
   
-  def initialize(name, grade)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
   end
